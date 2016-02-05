07b8fbdf759f2098e2953ca02b2d8464859955c8
washtubs <1@1>
2016-02-04 20:23:26 -0500

    update should be robust

--- scripts-CAUTION/update.sh
+++ scripts-CAUTION/update.sh
@@ -1,1 +1,4 @@
-git pull origin master
+git show-ref --verify --quiet refs/heads/$(uname -n) || git branch $(uname -n) master
+git checkout $(uname -n) && \
+git fetch origin master && \
+git merge -X theirs origin/master
